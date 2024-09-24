9912ca9628e1b8e120bf847eef9f359802205bd9
sjcode99 <surajlal261199@gmail.com>
2024-09-24 15:59:30 +0530

    refactored code and fixed some css

--- public/components/PromptModal.tsx
+++ public/components/PromptModal.tsx
@@ -1,78 +1,90 @@
-import React, { useState } from "react"
-import "../../style.css";
-// import { FaArrowDown } from "react-icons/fa6"
-// import { HiArrowPathRoundedSquare } from "react-icons/hi2"
-// import { LuSendHorizonal } from "react-icons/lu"
+import React, { useState } from "react";
+import InsertIcon from "../../assets/insert-icon.svg";
+import RegenerateIcon from "../../assets/regenerate-icon.svg";
+import GenerateIcon from "../../assets/generate-icon.svg";
 
 interface ModalProps {
-  isOpen: boolean
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
-  const [isGenerated, setIsGenerated] = useState(false)
-  const [userInput, setUserInput] = useState("")
-  const [regenerateInput, setRegenerateInput] = useState("")
+  const [isGenerated, setIsGenerated] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [regenerateInput, setRegenerateInput] = useState("");
 
-  const DUMMY_RESPONSE = `Thank you for the opportunity! If you have any more questions or if there's anythin else I can help you with, feel free to ask`
+  const DUMMY_RESPONSE = `Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.`;
 
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
   const handleOverlayClick = () => {
-    isOpen ? closeModal() : null
-  }
+    isOpen ? closeModal() : null;
+  };
 
   const handleInsert = () => {
-    closeModal()
+    closeModal();
 
     const textarea = document.querySelector(
       ".msg-form__contenteditable"
-    ) as HTMLTextAreaElement
+    ) as HTMLTextAreaElement;
 
-    textarea.children[0].innerHTML = DUMMY_RESPONSE
-    setIsGenerated(false)
-    setUserInput("")
-  }
+    const textarea_msg = document.querySelector(".msg-form__placeholder");
+    textarea_msg?.setAttribute("style", "display: none");
 
+    textarea.ariaLabel = "";
+    textarea.children[0].innerHTML = DUMMY_RESPONSE;
+    setIsGenerated(false);
+    setUserInput("");
+  };
 
-  console.log(isOpen);
   return (
     <>
       {isOpen && (
         <div
           className="fixed top-0 left-0 w-full h-full flex justify-center align-center inset-0 bg-black/25"
-          onClick={handleOverlayClick}>
+          onClick={handleOverlayClick}
+        >
           {!isGenerated ? (
             <div
               onClick={(e) => e.stopPropagation()}
-              className="flex flex-col items-center justify-center self-center bg-gray-50 p-5 w-[45rem] h-fit rounded-lg z-[9999]">
+              className="flex flex-col items-center justify-center self-center bg-gray-50 p-5 w-[45rem] h-fit rounded-lg z-[9999]"
+            >
               <input
                 type="text"
                 name="prompt"
                 placeholder="Your prompt"
                 value={userInput}
                 onChange={(e) => setUserInput(e.target.value)}
-                className="w-full focus:ring-0 focus:border-gray-300 border-gray-200 placeholder:text-gray-400 mb-6 text-gray-500 rounded-sm font-medium text-xl"
+                className="w-full focus:ring-slate-300 focus:border-slate-300 border border-slate-300 placeholder:text-gray-400 mb-6 text-gray-500 rounded-lg font-medium text-xl p-4"
               />
               <button
                 onClick={() => {
-                  setIsGenerated(true)
+                  setIsGenerated(true);
                 }}
                 disabled={!userInput}
-                className="flex items-center justify-around self-end bg-blue-500 text-white p-3 w-40 rounded-lg">
-                {/* <LuSendHorizonal className="text-2xl" /> */}
-
-                <p className="text-xl font-medium">Generate</p>
+                className={`flex items-center justify-around self-end bg-blue-500 text-white p-3 w-40 rounded-lg ${
+                  userInput.length > 0
+                    ? "cursor-pointer"
+                    : "cursor-not-allowed pointer-events-none"
+                }  `}
+              >
+                <p className="text-xl font-medium flex items-center gap-2">
+                  <span>
+                    <img src={GenerateIcon} alt="generate-icon" />
+                  </span>
+                  Generate
+                </p>
               </button>
             </div>
           ) : (
             <div
               onClick={(e) => e.stopPropagation()}
-              className="flex flex-col items-center justify-center self-center bg-gray-50 p-5 w-[45rem] h-fit rounded-lg gap-5">
+              className="flex flex-col items-center justify-center self-center bg-gray-50 p-5 w-[45rem] h-fit rounded-lg gap-5"
+            >
               {/* chats */}
-              <div className="flex flex-col gap-10 text-xl">
+              <div className="flex flex-col gap-6 text-xl">
                 {/* user inputed value */}
                 <p className="p-3 text-slate-500 bg-slate-200 rounded-lg self-end w-[70%]">
                   {userInput}
@@ -82,28 +94,25 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
                   {DUMMY_RESPONSE}
                 </p>
               </div>
-              <div className="w-full">
-                <input
-                  type="text"
-                  name="prompt"
-                  placeholder="Your prompt"
-                  value={regenerateInput}
-                  onChange={(e) => setRegenerateInput(e.target.value)}
-                  className="w-full focus:ring-0 focus:border-gray-300 border-gray-200 placeholder:text-gray-400 mb-6 text-gray-500 rounded-sm font-medium text-xl"
-                />
-              </div>
               <div className="flex items-center gap-5 self-end">
                 <button
                   onClick={() => handleInsert()}
-                  className="flex items-center justify-around self-end bg-white text-gray-500 p-3 w-32 rounded-lg border border-gray-500">
-                  {/* <FaArrowDown className="text-xl" /> */}
-
-                  <p className="text-xl font-medium">Insert</p>
+                  className="flex items-center justify-around self-end bg-white text-gray-500 p-3 w-32 rounded-lg border border-gray-500 cursor-pointer"
+                >
+                  <p className="text-xl font-medium flex items-center gap-2">
+                    <span>
+                      <img src={InsertIcon} alt="insert-icon" />
+                    </span>
+                    Insert
+                  </p>
                 </button>
-                <button className="flex items-center justify-around self-end bg-blue-500 text-white p-3 w-40 rounded-lg">
-                  {/* <HiArrowPathRoundedSquare className="text-xl" /> */}
-
-                  <p className="text-xl font-medium">Regenerate</p>
+                <button className="flex items-center justify-around self-end bg-blue-500 text-white p-3 w-40 rounded-lg cursor-pointer">
+                  <p className="text-xl font-medium flex items-center gap-2">
+                    <span>
+                      <img src={RegenerateIcon} alt="regenerate-icon" />
+                    </span>
+                    Regenerate
+                  </p>
                 </button>
               </div>
             </div>
@@ -111,7 +120,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
